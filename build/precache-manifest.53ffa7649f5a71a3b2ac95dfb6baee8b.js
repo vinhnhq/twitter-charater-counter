f31505aa07b474f3ef9b5f734acd1115649b5b79
Vinh Nguyen <nhquocvinh@gmail.com>
2019-12-19 16:00:14 +0700

    Initial commit from Create React App

---
+++ build/precache-manifest.53ffa7649f5a71a3b2ac95dfb6baee8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30996d4dd8a72db0446667275eb0594",
+    "url": "/index.html"
+  },
+  {
+    "revision": "180be34917cf7d11c0ad",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7ea382c9bf9b8debb47e",
+    "url": "/static/js/2.53b684b5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.53b684b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "180be34917cf7d11c0ad",
+    "url": "/static/js/main.fe43cbf3.chunk.js"
+  },
+  {
+    "revision": "f2e2d2f0ef5b5d71b5c4",
+    "url": "/static/js/runtime-main.1b05617f.js"
+  }
+]);
